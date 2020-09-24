026b955f5520316790a24a153ce66196596a1825
DimerDev <erasovwork@gmail.com>
2020-09-24 13:34:57 +0300

    Try creat Carousel

--- src/Pages/Inform.jsx
+++ src/Pages/Inform.jsx
@@ -4,7 +4,30 @@ import React from 'react';
 const Inform = () => {
    return (
       <div>
-         INFORMATION
+         <div>
+            INFORMATION
+      </div>
+         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut modi explicabo nam officiis, doloribus ipsam aspernatur necessitatibus amet! Deleniti sint commodi laboriosam. Asperiores aut reprehenderit a possimus perspiciatis similique earum.
+         Deserunt magni, porro sapiente sint nesciunt qui quas cupiditate libero alias perferendis amet fugiat sequi tempore officia blanditiis quod nobis maxime optio consequuntur. Inventore est aliquam consequatur aut, illo perspiciatis!
+         Quis libero quam aliquam expedita cum dolor aut eius ab! Voluptatum voluptas qui blanditiis magnam hic necessitatibus laboriosam veniam? Dicta, doloremque eum. Laudantium cumque iste dolorem suscipit placeat inventore modi.
+         Similique voluptatibus doloremque deleniti! Minus sit aliquid libero illo corporis placeat facere ullam harum voluptates quo porro voluptatem in autem officia rerum, totam inventore, ducimus enim nulla quas, beatae laboriosam.
+         Odit asperiores sapiente quae architecto quas voluptates rem. Dolorem saepe recusandae libero doloremque odit ab labore, soluta iste mollitia quisquam qui commodi quae earum porro magnam laudantium autem tenetur facere.
+         Expedita ea amet alias soluta voluptatem ipsum impedit deleniti doloribus pariatur veritatis, delectus laudantium placeat illo nobis animi? Laborum officia eveniet accusantium libero recusandae culpa, alias et rem quos vel!
+         Consequatur amet molestiae tempore. Nisi quidem ab animi quas facere corporis est consectetur eligendi voluptatum provident! Esse vel quam, quia temporibus corporis sit laborum repellat expedita nobis amet rem. Harum?
+         Reprehenderit, ipsa voluptatum? Sapiente, nisi porro non laborum eveniet modi consequatur assumenda voluptatibus rem. Necessitatibus corrupti qui eligendi commodi eius? Modi fugit neque voluptatem alias adipisci aliquam fuga optio illo.
+         Unde temporibus possimus asperiores eius perspiciatis repellendus minus natus fuga provident ab. Nam omnis minus hic, voluptates, impedit ex labore ea odio error iste mollitia dolorum inventore possimus eius! Fugit.
+         Ab ad rerum labore quis! Fugiat similique aperiam ipsam. Maiores porro iure fuga repudiandae illum facere deleniti cumque atque? Iusto, reprehenderit quaerat illum dicta accusamus aperiam esse eius magnam ipsum!
+         Quisquam, nisi? Voluptates quibusdam nostrum animi, fugiat, optio corporis dolor cupiditate vitae laboriosam quo minus eaque autem distinctio aut amet asperiores tenetur, sapiente hic dolorem? Hic voluptates quam perspiciatis ad.
+         Maxime accusamus animi eaque distinctio fugit temporibus nostrum saepe placeat ducimus repudiandae, provident odit impedit recusandae alias veniam reiciendis, iste quae, illum cumque. Fugiat, deserunt! Tempore voluptatem minus perspiciatis debitis!
+         Optio dolorum quas facere assumenda eligendi sapiente exercitationem vel enim. Nisi eos eius voluptas quas placeat laudantium aperiam expedita, magnam perspiciatis rem quibusdam laboriosam tenetur porro vero. Corporis, voluptate deleniti?
+         Minus repellat unde voluptatibus, quidem magnam deleniti dicta omnis! Iusto sit omnis officia ullam soluta dolorem eos optio rem quo blanditiis! Maxime inventore ex voluptas aliquam, vel fuga aut fugit.
+         Reprehenderit rerum nobis nemo, neque obcaecati debitis corrupti dolore sint ullam repellat natus, delectus sed dignissimos a assumenda quis saepe voluptatibus sequi alias dolorem. Corporis ipsa aliquam culpa reprehenderit aperiam.
+         In dolor similique, at placeat labore vero excepturi ducimus non atque laudantium quidem? Blanditiis illo maiores, sapiente cum eaque, debitis dolorem et animi pariatur reprehenderit repellendus voluptate consequatur eveniet minima!
+         Temporibus cum sed maxime voluptate beatae molestiae inventore fugiat dolores. Illum neque quidem mollitia, accusantium dolor libero consequatur quae tempora eveniet placeat atque laborum, eligendi vel impedit sit id provident!
+         Autem, vero tempore eius sit consequatur reprehenderit earum esse reiciendis illo ipsum atque mollitia deleniti? Cupiditate cumque tempore eligendi incidunt libero, ad nostrum deleniti, quas quisquam amet non ratione veritatis.
+         Quasi repellendus maiores accusantium et ipsam dolore cupiditate voluptatum molestiae velit quas suscipit, vel consequatur qui impedit cumque voluptates sint eligendi enim voluptatem incidunt ex est iste provident rerum? Exercitationem!
+         Laborum autem illo natus cupiditate inventore quaerat nulla quidem quibusdam modi, fugit repellendus. Error ullam recusandae culpa adipisci asperiores, similique ratione consequatur aliquid ipsa inventore modi corporis cupiditate enim placeat!
+      </div>
       </div>
    )
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import styl from './home.module.css'
+import s from './home.module.css'
 
 const Home = () => {
-   return (
-      <div className={styl.home}>
 
+
+
+   return (
+      <div className={s.home}>
+         <div className={s.bgItem}>1</div>
+         <div className={s.bgItem}>2</div>
+         <div className={s.bgItem}>3</div>
+         <div className={s.bgItem}>4</div>
+         <div className={s.bgItem}>5</div>
       </div>
    );
 }
+
 
 export default Home;
